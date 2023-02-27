df77ee74ad11d274cc5fe87444ef6d8d6ffd03b4
javipiazza94 <javipiazza94@gmail.com>
2023-02-27 02:11:20 +0100

    Terminamos el archivo de ejemplo de ficheros

--- Apuntes_Pildoras_Informaticas/Ficheros.py
+++ Apuntes_Pildoras_Informaticas/Ficheros.py
@@ -1,3 +1,17 @@
-f = open("archivo.txt", 'w')
-f.writelines("Viva Franco")
+#Creamos y escribimos el fichero
+f = open("archivo.txt", 'a+')
+f.writelines("Viva Franco\n")
+f.writelines("Arriba Espana\n")
+f.writelines("Una, Grande y Libre\n")
 f.close()
+
+#Lo leemos con with que cierra el fichero automaticamente
+with open("archivo.txt", 'r') as archivo: 
+    #contenido = archivo.read()
+    #puntero = archivo.tell()
+    #puntero_posicion = archivo.seek(5)
+    contenido_lineas = archivo.readlines()
+    #print(contenido)
+    #print(puntero)
+    #print(puntero_posicion)
+    print(contenido_lineas)
